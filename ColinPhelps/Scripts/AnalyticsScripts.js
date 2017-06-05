f6722f7d5a923cbba73d4fafc1c9e79aa4fd0d9a
colin.phelps <colin.phelps@zirmed.com>
2017-06-05 10:44:10 -0400

    fix analytics for resume button

--- ColinPhelps/Scripts/AnalyticsScripts.js
+++ ColinPhelps/Scripts/AnalyticsScripts.js
@@ -1,1 +1,10 @@
-﻿
+﻿$(document).ready(function () {
+
+
+    $('#resumeButton').click(function () {
+        ga('create', 'UA-100321309-1', 'auto');
+        ga('send', 'event', 'button', 'click', 'Resume');
+        alert("Resume button clicked");
+    });
+
+});
