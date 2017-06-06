c2ab70079e5513aef30e1f594661efdf92f91948
colin.phelps <colin.phelps@zirmed.com>
2017-06-06 13:19:02 -0400

    add error page

---
+++ ColinPhelps/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ColinPhelps.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public FileResult Index()
+        {
+            return File("Views/Error/Error.html","text/html");
+        }
+    }
+}
--- ColinPhelps/Global.asax.cs
+++ ColinPhelps/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using ColinPhelps.Controllers;
 
 namespace ColinPhelps
 {
@@ -14,5 +15,33 @@ namespace ColinPhelps
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
-    }
+
+        /*void Application_Error(object sender, EventArgs e)
+        {
+            // Grab information about the last error occurred 
+            var exception = Server.GetLastError();
+
+            // Clear the response stream 
+            var httpContext = ((HttpApplication)sender).Context;
+            httpContext.Response.Clear();
+            httpContext.ClearError();
+            httpContext.Response.TrySkipIisCustomErrors = true;
+
+            // Manage to display a friendly view 
+            InvokeErrorAction(httpContext, exception);
+
+        }
+        void InvokeErrorAction(HttpContext httpContext, Exception exception)
+        {
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "error";
+            routeData.Values["action"] = "index";
+            routeData.Values["exception"] = exception;
+            using (var controller = new ErrorController())
+            {
+                ((IController)controller).Execute(
+                new RequestContext(new HttpContextWrapper(httpContext), routeData));
+            }
+        }
+    */}
 }
