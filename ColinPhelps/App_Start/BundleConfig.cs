308ca05827eecee59867bcc46e3248eca6b74298
colin.phelps <colin.phelps@zirmed.com>
2017-06-06 16:45:28 -0400

    add module pattern, add bundleconfig

---
+++ ColinPhelps/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ColinPhelps.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.UseCdn = true;
+
+
+
+            BundleTable.EnableOptimizations = false;
+        }
+    }
+}
--- ColinPhelps/Scripts/AnalyticsScripts.js
+++ ColinPhelps/Scripts/AnalyticsScripts.js
@@ -1,20 +1,15 @@
-﻿(function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date(); a = s.createElement(o),
-    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+﻿var analyticsHelper = (function (jQuery) {
+    "use strict";
 
-ga('create', 'UA-100321309-1', 'auto');
-ga('send', 'pageview');
+    var module = {
+        onready: function () {
 
-$(document).ready(function () {
-
-
-    $('#resumeButton').click(function () {
-        ga('create', 'UA-100321309-1', 'auto');
-        ga('send', 'event', 'button', 'click', 'ResumeBtn');
-        alert("Resume button clicked");
-    });
-
-});
+                $('#resumeButton').click(function () {
+                    ga('send', 'event', 'button', 'click', 'ResumeBtn');
+                    alert("Resume button clicked");
+                });
+        }
+    }
+    return module;
+    // Any private functions should go below this line
+})(jQuery);
